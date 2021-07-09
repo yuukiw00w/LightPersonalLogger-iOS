3e11bb24fd26351d30b263646e740db7fc0ba2b9
yuki-noseda <yuki.noseda@dena.com>
2021-07-09 09:53:08 +0900

    Main.storyboardを開いたことが分かるようにLabelとAlertを追加（最初のVCだからか、viewWillAppearではまだAlertのpresentはできなかった）

--- LightPersonalLogger/Features/Main/ViewController.swift
+++ LightPersonalLogger/Features/Main/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        // 最初のVCだからか、ここではまだAlertのpresentはできなかった
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let alert = UIAlertController(title: "test", message: "message", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "ok", style: .default, handler: nil)
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
     }
 }
